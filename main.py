5e7527954847dcec24218416c26f61cac891c256
Alex0364106 <158524002+Alex0364106@users.noreply.github.com>
2024-02-07 09:34:05 -0800

    no change

--- main.py
+++ main.py
@@ -1,1 +1,225 @@
-# ...
+def makeorb():
+    global mySprite
+    mySprite = sprites.create(img("""
+            . . . . . . . . . . . . . . . .
+                    . . . . . . . . . . . . . . . .
+                    . . . . . . . . . . . . . . . .
+                    . . . . . . . . . . . . . . . .
+                    . . . . . . . . . . . . . . . .
+                    . . 2 2 2 2 2 2 2 2 2 2 2 2 . .
+                    . . 3 3 3 3 3 3 3 3 3 3 3 3 . .
+                    . . 1 1 1 1 1 1 1 1 1 1 1 1 . .
+                    . . 1 1 1 1 1 1 1 1 1 1 1 1 . .
+                    . . 3 3 3 3 3 3 3 3 3 3 3 3 . .
+                    . . 2 2 2 2 2 2 2 2 2 2 2 2 . .
+                    . . . . . . . . . . . . . . . .
+                    . . . . . . . . . . . . . . . .
+                    . . . . . . . . . . . . . . . .
+                    . . . . . . . . . . . . . . . .
+                    . . . . . . . . . . . . . . . .
+        """),
+        SpriteKind.projectile)
+    animation.run_image_animation(mySprite,
+        [img("""
+                . . . . . . . . . . . . . . . .
+                        . . . . . . . . . . . . . . . .
+                        . . . . . . . . . . . . . . . .
+                        . . . . . . . . . . . . . . . .
+                        . . . . . . . . . . . . . . . .
+                        . . 2 2 2 2 2 2 2 2 2 2 2 2 . .
+                        . . 3 3 3 3 3 3 3 3 3 3 3 3 . .
+                        . . 1 1 1 1 1 1 1 1 1 1 1 1 . .
+                        . . 1 1 1 1 1 1 1 1 1 1 1 1 . .
+                        . . 3 3 3 3 3 3 3 3 3 3 3 3 . .
+                        . . 2 2 2 2 2 2 2 2 2 2 2 2 . .
+                        . . . . . . . . . . . . . . . .
+                        . . . . . . . . . . . . . . . .
+                        . . . . . . . . . . . . . . . .
+                        . . . . . . . . . . . . . . . .
+                        . . . . . . . . . . . . . . . .
+            """),
+            img("""
+                . . . . . . . . . . . . . . . .
+                        . . . . . . . . . . . . . . . .
+                        . . . . . . . . . . . . . . . .
+                        . . . . . . . . . . . . . . . .
+                        . . . . . . . . . 2 2 2 2 . . .
+                        . . . . . . . 2 2 1 1 1 1 2 . .
+                        . . . . 2 2 3 3 1 1 1 1 1 1 . .
+                        . . 3 3 3 3 1 1 1 1 1 1 1 1 . .
+                        . . 1 1 1 1 1 1 1 1 1 1 1 1 . .
+                        . . 3 3 2 2 3 1 1 1 1 1 1 1 . .
+                        . . . . . . 2 2 3 1 1 1 1 2 . .
+                        . . . . . . . . . 2 2 2 2 . . .
+                        . . . . . . . . . . . . . . . .
+                        . . . . . . . . . . . . . . . .
+                        . . . . . . . . . . . . . . . .
+                        . . . . . . . . . . . . . . . .
+            """),
+            img("""
+                . . . . . . . . . . . . . . . .
+                        . . . . . . . . . . . . . . . .
+                        . . . . . . . 1 1 3 . . . . . .
+                        . . . . . . 1 3 . 3 3 . . . . .
+                        . . . . . . 1 . . . 3 2 2 3 . .
+                        . . . . . 1 3 . . . 2 2 1 3 3 .
+                        . . . . . 1 3 . 2 2 3 1 1 1 3 .
+                        . . 2 2 2 1 3 3 3 3 3 1 1 1 3 .
+                        . . 1 1 1 1 3 1 1 1 1 1 1 1 3 .
+                        . . 2 2 2 1 3 3 3 3 3 1 1 1 3 .
+                        . . . . . 1 3 . 2 2 3 1 1 1 3 .
+                        . . . . . 1 3 . . . 2 2 1 3 3 .
+                        . . . . . . 1 . . . 3 2 2 3 . .
+                        . . . . . . 1 3 . 3 3 . . . . .
+                        . . . . . . . 1 1 3 . . . . . .
+                        . . . . . . . . . . . . . . . .
+            """),
+            img("""
+                . . . . . . . . . . . . . . . .
+                        . . . . . 3 3 . . . 3 . . . . .
+                        . . . . 3 3 . . . . 3 3 . . . .
+                        . . . . 3 . . . . . . 3 3 . . .
+                        . . . . . . . . . . . . 3 . . .
+                        . . . . . . . . . . . . . . . .
+                        . . 3 . . . . . . . . . . . . .
+                        . . 3 . . . . . . . . . . 3 . .
+                        . . 3 . . . . . . . . . . 3 . .
+                        . . . . . . . . . . . . . 3 . .
+                        . . . . . . . . . . . . . . . .
+                        . . . 3 . . . . . . . . . . . .
+                        . . . 3 3 . . . . . . 3 . . . .
+                        . . . . 3 3 . . . . 3 3 . . . .
+                        . . . . . . . . . 3 3 . . . . .
+                        . . . . . . . . . . . . . . . .
+            """)],
+        500,
+        False)
+    mySprite.change_scale(0.111, ScaleAnchor.MIDDLE)
+    mySprite.set_position(playa.x, playa.y)
+    mySprite.set_velocity(50, 0)
+
+def on_a_pressed():
+    makeorb()
+controller.A.on_event(ControllerButtonEvent.PRESSED, on_a_pressed)
+
+def randomTilemap():
+    global tilemapList
+    tilemapList = [tilemap("""
+            level1
+        """),
+        tilemap("""
+            level2
+        """),
+        tilemap("""
+            level3
+        """),
+        tilemap("""
+            level4
+        """),
+        tilemap("""
+            level5
+        """)]
+    return tilemapList._pick_random()
+tilemapList: List[tiles.TileMapData] = []
+mySprite: Sprite = None
+playa: Sprite = None
+tiles.set_current_tilemap(randomTilemap())
+playa = sprites.create(img("""
+        . . . . . . f f f f f f . . . .
+            . . . . f f e e e e f 2 f . . .
+            . . . f f e e e e f 2 2 2 f . .
+            . . . f e e e f f e e e e f . .
+            . . . f f f f e e 2 2 2 2 e f .
+            . . . f e 2 2 2 f f f f e 2 f .
+            . . f f f f f f f e e e f f f .
+            . . f f e 4 4 e b f 4 4 e e f .
+            . . f e e 4 d 4 1 f d d e f . .
+            . . . f e e e 4 d d d d f . . .
+            . . . . f f e e 4 4 4 e f . . .
+            . . . . . 4 d d e 2 2 2 f . . .
+            . . . . . e d d e 2 2 2 f . . .
+            . . . . . f e e f 4 5 5 f . . .
+            . . . . . . f f f f f f . . . .
+            . . . . . . . f f f . . . . . .
+    """),
+    SpriteKind.player)
+animation.run_image_animation(playa,
+    [img("""
+            . . . . . . f f f f f f . . . .
+                . . . . f f e e e e f 2 f . . .
+                . . . f f e e e e f 2 2 2 f . .
+                . . . f e e e f f e e e e f . .
+                . . . f f f f e e 2 2 2 2 e f .
+                . . . f e 2 2 2 f f f f e 2 f .
+                . . f f f f f f f e e e f f f .
+                . . f f e 4 4 e b f 4 4 e e f .
+                . . f e e 4 d 4 1 f d d e f . .
+                . . . f e e e 4 d d d d f . . .
+                . . . . f f e e 4 4 4 e f . . .
+                . . . . . 4 d d e 2 2 2 f . . .
+                . . . . . e d d e 2 2 2 f . . .
+                . . . . . f e e f 4 5 5 f . . .
+                . . . . . . f f f f f f . . . .
+                . . . . . . . f f f . . . . . .
+        """),
+        img("""
+            . . . . . . . . . . . . . . . .
+                . . . . . . f f f f f f . . . .
+                . . . . f f e e e e f 2 f . . .
+                . . . f f e e e e f 2 2 2 f . .
+                . . . f e e e f f e e e e f . .
+                . . . f f f f e e 2 2 2 2 e f .
+                . . . f e 2 2 2 f f f f e 2 f .
+                . . f f f f f f f e e e f f f .
+                . . f f e 4 4 e b f 4 4 e e f .
+                . . f e e 4 d 4 1 f d d e f . .
+                . . . f e e e e e d d d f . . .
+                . . . . . f 4 d d e 4 e f . . .
+                . . . . . f e d d e 2 2 f . . .
+                . . . . f f f e e f 5 5 f f . .
+                . . . . f f f f f f f f f f . .
+                . . . . . f f . . . f f f . . .
+        """),
+        img("""
+            . . . . . . f f f f f f . . . .
+                . . . . f f e e e e f 2 f . . .
+                . . . f f e e e e f 2 2 2 f . .
+                . . . f e e e f f e e e e f . .
+                . . . f f f f e e 2 2 2 2 e f .
+                . . . f e 2 2 2 f f f f e 2 f .
+                . . f f f f f f f e e e f f f .
+                . . f f e 4 4 e b f 4 4 e e f .
+                . . f e e 4 d 4 1 f d d e f . .
+                . . . f e e e 4 d d d d f . . .
+                . . . . f f e e 4 4 4 e f . . .
+                . . . . . 4 d d e 2 2 2 f . . .
+                . . . . . e d d e 2 2 2 f . . .
+                . . . . . f e e f 4 5 5 f . . .
+                . . . . . . f f f f f f . . . .
+                . . . . . . . f f f . . . . . .
+        """),
+        img("""
+            . . . . . . . . . . . . . . . .
+            . . . . . . f f f f f f . . . .
+            . . . . f f e e e e f 2 f . . .
+            . . . f f e e e e f 2 2 2 f . .
+            . . . f e e e f f e e e e f . .
+            . . . f f f f e e 2 2 2 2 e f .
+            . . . f e 2 2 2 f f f f e 2 f .
+            . . f f f f f f f e e e f f f .
+            . . f f e 4 4 e b f 4 4 e e f .
+            . . f e e 4 d 4 1 f d d e f . .
+            . . . f e e e 4 d d d d f . . .
+            . . . . 4 d d e 4 4 4 e f . . .
+            . . . . e d d e 2 2 2 2 f . . .
+            . . . . f e e f 4 4 5 5 f f . .
+            . . . . f f f f f f f f f f . .
+            . . . . . f f . . . f f f . . .
+        """)],
+    500,
+    True)
+playa.set_stay_in_screen(True)
+scene.camera_follow_sprite(playa)
+controller.move_sprite(playa)
+statusbar = statusbars.create(4, 10, StatusBarKind.health)
+statusbar.attach_to_sprite(playa)
--- main.ts
+++ main.ts
@@ -1,1 +1,209 @@
+function makeorb() {
+    
+    mySprite = sprites.create(img`
+            . . . . . . . . . . . . . . . .
+                    . . . . . . . . . . . . . . . .
+                    . . . . . . . . . . . . . . . .
+                    . . . . . . . . . . . . . . . .
+                    . . . . . . . . . . . . . . . .
+                    . . 2 2 2 2 2 2 2 2 2 2 2 2 . .
+                    . . 3 3 3 3 3 3 3 3 3 3 3 3 . .
+                    . . 1 1 1 1 1 1 1 1 1 1 1 1 . .
+                    . . 1 1 1 1 1 1 1 1 1 1 1 1 . .
+                    . . 3 3 3 3 3 3 3 3 3 3 3 3 . .
+                    . . 2 2 2 2 2 2 2 2 2 2 2 2 . .
+                    . . . . . . . . . . . . . . . .
+                    . . . . . . . . . . . . . . . .
+                    . . . . . . . . . . . . . . . .
+                    . . . . . . . . . . . . . . . .
+                    . . . . . . . . . . . . . . . .
+        `, SpriteKind.Projectile)
+    animation.runImageAnimation(mySprite, [img`
+                . . . . . . . . . . . . . . . .
+                        . . . . . . . . . . . . . . . .
+                        . . . . . . . . . . . . . . . .
+                        . . . . . . . . . . . . . . . .
+                        . . . . . . . . . . . . . . . .
+                        . . 2 2 2 2 2 2 2 2 2 2 2 2 . .
+                        . . 3 3 3 3 3 3 3 3 3 3 3 3 . .
+                        . . 1 1 1 1 1 1 1 1 1 1 1 1 . .
+                        . . 1 1 1 1 1 1 1 1 1 1 1 1 . .
+                        . . 3 3 3 3 3 3 3 3 3 3 3 3 . .
+                        . . 2 2 2 2 2 2 2 2 2 2 2 2 . .
+                        . . . . . . . . . . . . . . . .
+                        . . . . . . . . . . . . . . . .
+                        . . . . . . . . . . . . . . . .
+                        . . . . . . . . . . . . . . . .
+                        . . . . . . . . . . . . . . . .
+            `, img`
+                . . . . . . . . . . . . . . . .
+                        . . . . . . . . . . . . . . . .
+                        . . . . . . . . . . . . . . . .
+                        . . . . . . . . . . . . . . . .
+                        . . . . . . . . . 2 2 2 2 . . .
+                        . . . . . . . 2 2 1 1 1 1 2 . .
+                        . . . . 2 2 3 3 1 1 1 1 1 1 . .
+                        . . 3 3 3 3 1 1 1 1 1 1 1 1 . .
+                        . . 1 1 1 1 1 1 1 1 1 1 1 1 . .
+                        . . 3 3 2 2 3 1 1 1 1 1 1 1 . .
+                        . . . . . . 2 2 3 1 1 1 1 2 . .
+                        . . . . . . . . . 2 2 2 2 . . .
+                        . . . . . . . . . . . . . . . .
+                        . . . . . . . . . . . . . . . .
+                        . . . . . . . . . . . . . . . .
+                        . . . . . . . . . . . . . . . .
+            `, img`
+                . . . . . . . . . . . . . . . .
+                        . . . . . . . . . . . . . . . .
+                        . . . . . . . 1 1 3 . . . . . .
+                        . . . . . . 1 3 . 3 3 . . . . .
+                        . . . . . . 1 . . . 3 2 2 3 . .
+                        . . . . . 1 3 . . . 2 2 1 3 3 .
+                        . . . . . 1 3 . 2 2 3 1 1 1 3 .
+                        . . 2 2 2 1 3 3 3 3 3 1 1 1 3 .
+                        . . 1 1 1 1 3 1 1 1 1 1 1 1 3 .
+                        . . 2 2 2 1 3 3 3 3 3 1 1 1 3 .
+                        . . . . . 1 3 . 2 2 3 1 1 1 3 .
+                        . . . . . 1 3 . . . 2 2 1 3 3 .
+                        . . . . . . 1 . . . 3 2 2 3 . .
+                        . . . . . . 1 3 . 3 3 . . . . .
+                        . . . . . . . 1 1 3 . . . . . .
+                        . . . . . . . . . . . . . . . .
+            `, img`
+                . . . . . . . . . . . . . . . .
+                        . . . . . 3 3 . . . 3 . . . . .
+                        . . . . 3 3 . . . . 3 3 . . . .
+                        . . . . 3 . . . . . . 3 3 . . .
+                        . . . . . . . . . . . . 3 . . .
+                        . . . . . . . . . . . . . . . .
+                        . . 3 . . . . . . . . . . . . .
+                        . . 3 . . . . . . . . . . 3 . .
+                        . . 3 . . . . . . . . . . 3 . .
+                        . . . . . . . . . . . . . 3 . .
+                        . . . . . . . . . . . . . . . .
+                        . . . 3 . . . . . . . . . . . .
+                        . . . 3 3 . . . . . . 3 . . . .
+                        . . . . 3 3 . . . . 3 3 . . . .
+                        . . . . . . . . . 3 3 . . . . .
+                        . . . . . . . . . . . . . . . .
+            `], 500, false)
+    mySprite.changeScale(0.111, ScaleAnchor.Middle)
+    mySprite.setPosition(playa.x, playa.y)
+    mySprite.setVelocity(50, 0)
+}
 
+controller.A.onEvent(ControllerButtonEvent.Pressed, function on_a_pressed() {
+    makeorb()
+})
+function randomTilemap(): tiles.TileMapData {
+    
+    tilemapList = [tilemap`
+            level1
+        `, tilemap`
+            level2
+        `, tilemap`
+            level3
+        `, tilemap`
+            level4
+        `, tilemap`
+            level5
+        `]
+    return tilemapList._pickRandom()
+}
+
+let tilemapList : tiles.TileMapData[] = []
+let mySprite : Sprite = null
+let playa : Sprite = null
+tiles.setCurrentTilemap(randomTilemap())
+playa = sprites.create(img`
+        . . . . . . f f f f f f . . . .
+            . . . . f f e e e e f 2 f . . .
+            . . . f f e e e e f 2 2 2 f . .
+            . . . f e e e f f e e e e f . .
+            . . . f f f f e e 2 2 2 2 e f .
+            . . . f e 2 2 2 f f f f e 2 f .
+            . . f f f f f f f e e e f f f .
+            . . f f e 4 4 e b f 4 4 e e f .
+            . . f e e 4 d 4 1 f d d e f . .
+            . . . f e e e 4 d d d d f . . .
+            . . . . f f e e 4 4 4 e f . . .
+            . . . . . 4 d d e 2 2 2 f . . .
+            . . . . . e d d e 2 2 2 f . . .
+            . . . . . f e e f 4 5 5 f . . .
+            . . . . . . f f f f f f . . . .
+            . . . . . . . f f f . . . . . .
+    `, SpriteKind.Player)
+animation.runImageAnimation(playa, [img`
+            . . . . . . f f f f f f . . . .
+                . . . . f f e e e e f 2 f . . .
+                . . . f f e e e e f 2 2 2 f . .
+                . . . f e e e f f e e e e f . .
+                . . . f f f f e e 2 2 2 2 e f .
+                . . . f e 2 2 2 f f f f e 2 f .
+                . . f f f f f f f e e e f f f .
+                . . f f e 4 4 e b f 4 4 e e f .
+                . . f e e 4 d 4 1 f d d e f . .
+                . . . f e e e 4 d d d d f . . .
+                . . . . f f e e 4 4 4 e f . . .
+                . . . . . 4 d d e 2 2 2 f . . .
+                . . . . . e d d e 2 2 2 f . . .
+                . . . . . f e e f 4 5 5 f . . .
+                . . . . . . f f f f f f . . . .
+                . . . . . . . f f f . . . . . .
+        `, img`
+            . . . . . . . . . . . . . . . .
+                . . . . . . f f f f f f . . . .
+                . . . . f f e e e e f 2 f . . .
+                . . . f f e e e e f 2 2 2 f . .
+                . . . f e e e f f e e e e f . .
+                . . . f f f f e e 2 2 2 2 e f .
+                . . . f e 2 2 2 f f f f e 2 f .
+                . . f f f f f f f e e e f f f .
+                . . f f e 4 4 e b f 4 4 e e f .
+                . . f e e 4 d 4 1 f d d e f . .
+                . . . f e e e e e d d d f . . .
+                . . . . . f 4 d d e 4 e f . . .
+                . . . . . f e d d e 2 2 f . . .
+                . . . . f f f e e f 5 5 f f . .
+                . . . . f f f f f f f f f f . .
+                . . . . . f f . . . f f f . . .
+        `, img`
+            . . . . . . f f f f f f . . . .
+                . . . . f f e e e e f 2 f . . .
+                . . . f f e e e e f 2 2 2 f . .
+                . . . f e e e f f e e e e f . .
+                . . . f f f f e e 2 2 2 2 e f .
+                . . . f e 2 2 2 f f f f e 2 f .
+                . . f f f f f f f e e e f f f .
+                . . f f e 4 4 e b f 4 4 e e f .
+                . . f e e 4 d 4 1 f d d e f . .
+                . . . f e e e 4 d d d d f . . .
+                . . . . f f e e 4 4 4 e f . . .
+                . . . . . 4 d d e 2 2 2 f . . .
+                . . . . . e d d e 2 2 2 f . . .
+                . . . . . f e e f 4 5 5 f . . .
+                . . . . . . f f f f f f . . . .
+                . . . . . . . f f f . . . . . .
+        `, img`
+            . . . . . . . . . . . . . . . .
+            . . . . . . f f f f f f . . . .
+            . . . . f f e e e e f 2 f . . .
+            . . . f f e e e e f 2 2 2 f . .
+            . . . f e e e f f e e e e f . .
+            . . . f f f f e e 2 2 2 2 e f .
+            . . . f e 2 2 2 f f f f e 2 f .
+            . . f f f f f f f e e e f f f .
+            . . f f e 4 4 e b f 4 4 e e f .
+            . . f e e 4 d 4 1 f d d e f . .
+            . . . f e e e 4 d d d d f . . .
+            . . . . 4 d d e 4 4 4 e f . . .
+            . . . . e d d e 2 2 2 2 f . . .
+            . . . . f e e f 4 4 5 5 f f . .
+            . . . . f f f f f f f f f f . .
+            . . . . . f f . . . f f f . . .
+        `], 500, true)
+playa.setStayInScreen(true)
+scene.cameraFollowSprite(playa)
+controller.moveSprite(playa)
+let statusbar = statusbars.create(4, 10, StatusBarKind.Health)
+statusbar.attachToSprite(playa)
