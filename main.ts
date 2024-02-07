607b0bf6e2dc483c57c3ce28c65c252ecc621fc9
Evanbac <159074871+Evanbac@users.noreply.github.com>
2024-02-07 09:16:31 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,25 @@
+function makeorb () {
+    mySprite = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . 6 6 6 6 . . . . . . 
+        . . . . 6 6 6 5 5 6 6 6 . . . . 
+        . . . 7 7 7 7 6 6 6 6 6 6 . . . 
+        . . 6 7 7 7 7 8 8 8 1 1 6 6 . . 
+        . . 7 7 7 7 7 8 8 8 1 1 5 6 . . 
+        . 6 7 7 7 7 8 8 8 8 8 5 5 6 6 . 
+        . 6 7 7 7 8 8 8 6 6 6 6 5 6 6 . 
+        . 6 6 7 7 8 8 6 6 6 6 6 6 6 6 . 
+        . 6 8 7 7 8 8 6 6 6 6 6 6 6 6 . 
+        . . 6 8 7 7 8 6 6 6 6 6 8 6 . . 
+        . . 6 8 8 7 8 8 6 6 6 8 6 6 . . 
+        . . . 6 8 8 8 8 8 8 8 8 6 . . . 
+        . . . . 6 6 8 8 8 8 6 6 . . . . 
+        . . . . . . 6 6 6 6 . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Projectile)
+    mySprite.setPosition(playa.x, playa.x)
+    mySprite.setVelocity(50, 0)
+}
 function randomTilemap () {
     tilemapList = [
     tilemap`level1`,
@@ -9,8 +31,10 @@ function randomTilemap () {
     return tilemapList._pickRandom()
 }
 let tilemapList: tiles.TileMapData[] = []
+let mySprite: Sprite = null
+let playa: Sprite = null
 tiles.setCurrentTilemap(randomTilemap())
-let playa = sprites.create(img`
+playa = sprites.create(img`
     . . . . . . f f f f f f . . . . 
     . . . . f f e e e e f 2 f . . . 
     . . . f f e e e e f 2 2 2 f . . 
